e348ea9151e23da3c3a137070e1a79da5a3be2ed
kerolossarofem <47207049+kerolossarofem@users.noreply.github.com>
2021-11-03 21:22:06 -0400

    Update JobCoinMixer.java

--- src/main/java/coinMixer/JobCoinMixer.java
+++ src/main/java/coinMixer/JobCoinMixer.java
@@ -8,14 +8,13 @@ import java.util.concurrent.TimeUnit;
 import coinMixer.service.JobMixerService;
 import coinMixer.service.JobMixerServiceImpl;
 
-public class JobCoinMixer {
-	public static final String url = "http://jobcoin.gemini.com/negative/api/";
+public class JobCoinMixer extends Thread {
 	public static final String house_account = "House";
 	public static final BigDecimal FEE = BigDecimal.valueOf(0.015);
 	private static JobMixerService jobMixerService = new JobMixerServiceImpl();
 
 	public static void main(String args[]) throws Exception {
-		// Step 1: System will ask user for 3 unused addresses
+		// Step 1: System will ask user to provide unused addresses
 		List<String> addresses = userAddresses();
 
 		// Step 2: System chooses a deposit address
@@ -23,16 +22,24 @@ public class JobCoinMixer {
 		System.out.println("Please deposit your jobcoins to the following address: " + depositAddress);
 		System.out.println("A 1.5% fee will be taken out");
 
-		// Step 3: then checks to see if deposit has occurred
-		String balance = getDepositInfo(depositAddress);
-		System.out.println("Your deposit was a success!");
+		// run step 3-5 into a different thread.
+		new Thread(() -> { // Lambda Expression
+			try {
+				// Step 3: then checks to see if deposit has occurred
+				String balance = getDepositInfo(depositAddress);
+				System.out.println("Your deposit was a success!");
 
-		// Step 4: deposit is moved to house_account after 3-10 seconds
-		postValue(depositAddress, house_account, balance);
+				// Step 4: deposit is moved to house_account
+				sendCoin(depositAddress, house_account, balance);
+				System.out.println("Your deposit was moved to house account!");
 
-		// Step 5: Different incrememnts of the balance is moved from the house account
-		// to three different adddresses provided by the user
-		distributeFunds(balance, addresses);
+				// Step 5: Different increments of the balance is moved from the house account
+				distributeFunds(balance, addresses);
+			} catch (Exception e) {
+				System.out.println("ERROR: " + e);
+			}
+		}).start();
+		System.out.println("main Thread");
 
 	}
 
@@ -56,7 +63,8 @@ public class JobCoinMixer {
 		return UUID.randomUUID().toString();
 	}
 
-//getDepositInfo() makes a get request and checks the deposit account every 8 seconds to see if deposit has been made
+	// makes a get request and get balance!
+	// if the balance still 0, thried will wait for 5 sec and try again
 	public static String getDepositInfo(String depositAddress) throws Exception {
 		String balance = "0";
 		do {
@@ -66,7 +74,8 @@ public class JobCoinMixer {
 		return jobMixerService.getDeposit(depositAddress);
 	}
 
-//distributeFunds() takes fees and breaks the deposit into 3 different deposits and sends the parameters to postValue()
+	// takes fees and breaks the deposit into small deposits and sends the
+	// parameters to sendcoin
 	public static void distributeFunds(String balance, List<String> addresses) throws Exception {
 		// double currBalance = (Integer.parseInt(balance)) * 1.0;
 		BigDecimal currBalance = new BigDecimal(balance);
@@ -76,20 +85,20 @@ public class JobCoinMixer {
 		// nextInt as provided by Random is exclusive of the top value so you need to
 		// add 1
 		int min = addresses.size();
-		int max = min*3;
+		int max = min * 3;
 		for (int i = 0; i < addresses.size() - 1; i++) {
 			int randomNum = rand.nextInt((max - min) + 1) + min;
-			BigDecimal balance1 = currBalance.divide(new BigDecimal(randomNum),  MathContext.DECIMAL128);
+			BigDecimal balance1 = currBalance.divide(new BigDecimal(randomNum), MathContext.DECIMAL128);
 			currBalance = currBalance.subtract(balance1);
 			TimeUnit.SECONDS.sleep(5);
-			postValue(house_account, addresses.get(i), String.valueOf(balance1));
+			sendCoin(house_account, addresses.get(i), String.valueOf(balance1));
 		}
-		postValue(house_account, addresses.get(addresses.size() - 1), String.valueOf(currBalance));
+		sendCoin(house_account, addresses.get(addresses.size() - 1), String.valueOf(currBalance));
 	}
 
-//postValue takes in the 3 values needed for a transaction, and completes the transaction
-	public static void postValue(String from, String to, String balance) throws Exception {
-		jobMixerService.postValue(from, to, balance);
+	// sendCoin send amount from an address to another address
+	public static void sendCoin(String from, String to, String balance) throws Exception {
+		jobMixerService.sendCoin(from, to, balance);
 	}
 
 }
